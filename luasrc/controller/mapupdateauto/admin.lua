04447ca013a2bc9aa96f11b505ce56f24591665b
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2012-11-02 14:46:48 +0700

    Add feature to track service status in Admin page.

--- luasrc/controller/mapupdateauto/admin.lua
+++ luasrc/controller/mapupdateauto/admin.lua
@@ -6,4 +6,18 @@ function index()
 	      'Map Update')
 
 	entry({'admin', 'services', 'mapupdateauto', 'server'}, cbi('mapupdateauto/server'), 'Server')
+	entry({'admin', 'services', 'mapupdateauto', 'service_status'}, call('get_service_status'))
+end
+
+function get_service_status()
+	local process_name = 'updater.lua'
+	local fullpath = '/usr/bin/lua /etc/mapupdateauto/%s' % {process_name}
+	local out = luci.util.exec('ps aux | grep %s' % {process_name})
+	if out:find(fullpath, 1, true) then
+		stt = 'ON'
+	else
+		stt = 'OFF'
+	end
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({status = stt})
 end
--- luasrc/model/cbi/mapupdateauto/server.lua
+++ luasrc/model/cbi/mapupdateauto/server.lua
@@ -6,4 +6,7 @@ p = s:option(Value, 'host_url', 'Host URL',
              translate('URL to the a CouchDB host than running Map. <br/>Ex: http://192.168.1.182:5984/altermap'))
 p = s:option(Value, 'nid', 'Node ID')
 
+p = s:option(DummyValue, 'running', 'Service status')
+p.template = 'mapupdateauto/status'
+
 return m
